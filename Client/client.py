961957303eb4bf68684b69cfc8b90818d285dbb1
Bill Vineyard <71736216+wvineyard@users.noreply.github.com>
2021-12-26 16:15:28 -0600

    c# server working
    title, tested with a c# client example.

--- Client/client.py
+++ Client/client.py
@@ -1,1 +1,8 @@
-print("Hello World!")
+import socket
+
+s = socket.socket()
+host = '127.0.0.1'
+port = 11000
+s.connect((host, port))
+print(s.recv(1024))
+s.close()
--- Server/Server/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-static void Main()
-{
-    Console.WriteLine("Hello!");
-}

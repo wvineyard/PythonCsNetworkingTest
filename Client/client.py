0fca04b96cb669d5838b04a02f7d1b3527c6b8f9
Bill Vineyard <71736216+wvineyard@users.noreply.github.com>
2021-12-26 17:00:56 -0600

    python-cs client working
    nice yo.

--- Client/client.py
+++ Client/client.py
@@ -2,7 +2,10 @@ import socket
 
 s = socket.socket()
 host = '127.0.0.1'
-port = 11000
+port = 8888
+byte_message = bytes("hello world!", "utf-8")
 s.connect((host, port))
-print(s.recv(1024))
+s.sendto(byte_message, (host, port))
+message = s.recv(256)
+print(bytes.decode(message, 'utf-8'))
 s.close()
--- Server/Server/ServerTest.cs
+++ Server/Server/ServerTest.cs
@@ -1,160 +1,79 @@
-﻿using System;  
-using System.Net;  
-using System.Net.Sockets;  
-using System.Text;  
-using System.Threading;  
-  
-// State object for reading client data asynchronously  
-public class StateObject
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class MyTcpListener
 {
-    // Size of receive buffer.  
-    public const int BufferSize = 1024;
-
-    // Receive buffer.  
-    public byte[] buffer = new byte[BufferSize];
-
-    // Received data string.
-    public StringBuilder sb = new StringBuilder();
-
-    // Client socket.
-    public Socket workSocket = null;
-}  
-  
-public class AsynchronousSocketListener
-{
-    // Thread signal.  
-    public static ManualResetEvent allDone = new ManualResetEvent(false);
-
-    public AsynchronousSocketListener()
+    public static void Main()
     {
-    }
-
-    public static void StartListening()
-    {
-        // Establish the local endpoint for the socket.  
-        // The DNS name of the computer  
-        // running the listener is "host.contoso.com".  
-        IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());  
-        IPAddress ipAddress = ipHostInfo.AddressList[0];  
-        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);  
-  
-        // Create a TCP/IP socket.  
-        Socket listener = new Socket(ipAddress.AddressFamily,  
-            SocketType.Stream, ProtocolType.Tcp );  
-  
-        // Bind the socket to the local endpoint and listen for incoming connections.  
-        try {  
-            listener.Bind(localEndPoint);  
-            listener.Listen(100);  
-  
-            while (true) {  
-                // Set the event to nonsignaled state.  
-                allDone.Reset();  
-  
-                // Start an asynchronous socket to listen for connections.  
-                Console.WriteLine("Waiting for a connection...");  
-                listener.BeginAccept(
-                    new AsyncCallback(AcceptCallback),  
-                    listener );  
-  
-                // Wait until a connection is made before continuing.  
-                allDone.WaitOne();  
-            }  
-  
-        } catch (Exception e) {  
-            Console.WriteLine(e.ToString());  
-        }  
-  
-        Console.WriteLine("\nPress ENTER to continue...");  
-        Console.Read();  
-  
-    }
-
-    public static void AcceptCallback(IAsyncResult ar)
-    {
-        // Signal the main thread to continue.  
-        allDone.Set();  
-  
-        // Get the socket that handles the client request.  
-        Socket listener = (Socket) ar.AsyncState;  
-        Socket handler = listener.EndAccept(ar);  
-  
-        // Create the state object.  
-        StateObject state = new StateObject();  
-        state.workSocket = handler;  
-        handler.BeginReceive( state.buffer, 0, StateObject.BufferSize, 0,  
-            new AsyncCallback(ReadCallback), state);  
-    }
-
-    public static void ReadCallback(IAsyncResult ar)
-    {
-        String content = String.Empty;  
-  
-        // Retrieve the state object and the handler socket  
-        // from the asynchronous state object.  
-        StateObject state = (StateObject) ar.AsyncState;  
-        Socket handler = state.workSocket;  
-  
-        // Read data from the client socket.
-        int bytesRead = handler.EndReceive(ar);  
-  
-        if (bytesRead > 0) {  
-            // There  might be more data, so store the data received so far.  
-            state.sb.Append(Encoding.ASCII.GetString(  
-                state.buffer, 0, bytesRead));  
-  
-            // Check for end-of-file tag. If it is not there, read
-            // more data.  
-            content = state.sb.ToString();  
-            if (content.IndexOf("<EOF>") > -1) {  
-                // All the data has been read from the
-                // client. Display it on the console.  
-                Console.WriteLine("Read {0} bytes from socket. \n Data : {1}",  
-                    content.Length, content );  
-                // Echo the data back to the client.  
-                Send(handler, content);  
-            } else {  
-                // Not all data received. Get more.  
-                handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,  
-                new AsyncCallback(ReadCallback), state);  
-            }  
-        }  
-    }
-
-    private static void Send(Socket handler, String data)
-    {
-        // Convert the string data to byte data using ASCII encoding.  
-        byte[] byteData = Encoding.ASCII.GetBytes(data);  
-  
-        // Begin sending the data to the remote device.  
-        handler.BeginSend(byteData, 0, byteData.Length, 0,  
-            new AsyncCallback(SendCallback), handler);  
-    }
-
-    private static void SendCallback(IAsyncResult ar)
-    {
+        TcpListener server=null;
         try
         {
-            // Retrieve the socket from the state object.  
-            Socket handler = (Socket) ar.AsyncState;  
-  
-            // Complete sending the data to the remote device.  
-            int bytesSent = handler.EndSend(ar);  
-            Console.WriteLine("Sent {0} bytes to client.", bytesSent);  
-  
-            handler.Shutdown(SocketShutdown.Both);  
-            handler.Close();  
-  
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e.ToString());  
-        }  
-    }
+            // Set the TcpListener on port 13000.
+            Int32 port = 8888;
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
 
-    public static int Main(String[] args)
-    {
-        StartListening();  
-        return 0;  
+            // TcpListener server = new TcpListener(port);
+            server = new TcpListener(localAddr, port);
+
+            // Start listening for client requests.
+            server.Start();
+
+            // Buffer for reading data
+            Byte[] bytes = new Byte[256];
+            String data = null;
+
+            // Enter the listening loop.
+            while(true)
+            {
+                Console.Write("Waiting for a connection... ");
+
+                // Perform a blocking call to accept requests.
+                // You could also use server.AcceptSocket() here.
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Connected!");
+
+                data = null;
+
+                // Get a stream object for reading and writing
+                NetworkStream stream = client.GetStream();
+
+                int i;
+
+                // Loop to receive all the data sent by the client.
+                while((i = stream.Read(bytes, 0, bytes.Length))!=0)
+                {
+                    // Translate data bytes to a ASCII string.
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                    Console.WriteLine("Received: {0}", data);
+
+                    // Process the data sent by the client.
+                    data = data.ToUpper();
+
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                    // Send back a response.
+                    stream.Write(msg, 0, msg.Length);
+                    Console.WriteLine("Sent: {0}", data);
+                }
+
+                // Shutdown and end connection
+                client.Close();
+            }
+        }
+        catch(SocketException e)
+        {
+            Console.WriteLine("SocketException: {0}", e);
+        }
+        finally
+        {
+            // Stop listening for new clients.
+            server.Stop();
+        }
+
+        Console.WriteLine("\nHit enter to continue...");
+        Console.Read();
     }
 }
